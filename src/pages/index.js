efca4682078fb23bac3d8f20be169c4c909ffa05
devsentap <devsentap@gmail.com>
2020-02-16 23:03:00 +0800

    Able to insert customer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
+import Helmet from "react-helmet";
 import axios from "axios";
-import {
-  Section, Container, Table, Field, Label, Control, Input,
-  FieldLabel, Content, Title, Heading, Button, Select, Subtitle, Help
-} from "bloomer";
-import 'bulma/css/bulma.min.css';
+import { useForm } from "react-hook-form";
+
+import { Section, Container, Table, Field, Label, Control, Title, Button, Subtitle, Help } from "bloomer";
+import "bulma/css/bulma.min.css";
 
 const IndexPage = () => {
-  const [customers,      setCustomers]      = useState([]);
-  const [lastUpdated,    setLastUpdated]     = useState('Loading...');
+  const { register, handleSubmit, setValue, reset, errors } = useForm();
+  const [customers,        setCustomers]        = useState([]);
+  const [disableTxtEmail,  setDisableTxtEmail]  = useState(false);
+  const [disableBtnAdd,    setDisableBtnAdd]    = useState(false);
+  const [disableBtnUpdate, setDisableBtnUpdate] = useState(true);
+  const [disableBtnRemove, setDisableBtnRemove] = useState(true);
+  const [lastUpdated,      setLastUpdated]      = useState('Loading...');
 
   useEffect(() => {
     getCustomers();    
@@ -22,13 +27,39 @@ const IndexPage = () => {
     setLastUpdated(new Date().toUTCString());
   }
 
-  async function insertCustomer(name, email, mobile, address, product_id, source) {
-    //const email = document.getElementById('txtEmail');
-    // const url = `https://funcfreelancers.azurewebsites.net/api/insertFreelancer?code=lV3/AwrfLU/eDIo1m1zwfkFNKkNhU/2WzKVRQDh/3Nyi3LijJluPEg==&id=${id}&email=${email}&username=${username}&phone=${phone}&skills=${skills}&hobby=${hobby}`;
-    // const result = await axios(url);
-    // console.log('result', result);
-    // setFreelancer(result.data);
+  async function insertCustomer(data, e) {
+    console.log('insertCustomer', data);
+    const id          = data.txtEmail.trim();
+    const email       = data.txtEmail.trim();
+    const name        = data.txtName.trim();
+    const mobile      = data.txtMobile.trim();
+    const address     = data.txtAddress.trim();
+    const product_ids = data.txtProductIDs.trim();
+    const source      = data.ddSource.trim();    
+    const created     = new Date().toUTCString();
+
+    reset({
+      txtEmail:      '',
+      txtName:       '',
+      txtMobile:     '',
+      txtAddress:    '',
+      txtProductIDs: ''      
+    });
+
+    const url = `https://funccrm.azurewebsites.net/api/insertCustomer?code=caODt9/saABTSUsx0c52fxKLWHvznE1xeFaNXlLrhe2v4uCVS1mwBg==&id=${id}&email=${email}&name=${name}&mobile=${mobile}&address=${address}&product_ids=${product_ids}&source=${source}&created=${created}`;
+    console.log('url', url);
+    try {
+      const result = await axios.post(url);
+      getCustomers();
+      console.log('result insertCustomer', result);
+    } catch (error) {
+      console.log('[ERROR]', error);
+    }
   }
+
+  async function updateCustomer(data, e) {}
+
+  async function removeCustomer(data, e) {}
 
   function renderColumnNames() {
     return (
@@ -75,65 +106,69 @@ const IndexPage = () => {
 
   return (
     <Section>
+      <Helmet title="Malaysian Customer 2020 Application (Demo Version)"></Helmet>
       <Container>
         <Title isSize={4}>New Customer Info</Title>
         <Field>
           <Label>Name</Label>
           <Control>
-            <Input id="txtName" style={{ width: '50%' }}></Input>
+            <input name="txtName" className="input" type="text" style={{ width: '50%' }} ref={register({ required: true })} />            
           </Control>
+          { errors.txtName && <Help isColor='danger'>This field is required</Help> }
         </Field>
         <Field>
           <Label>Email</Label>
           <Control>
-            <Input id="txtEmail" style={{ width: '50%' }}></Input>
+            <input name="txtEmail" className="input" type="email" style={{ width: '50%' }} disabled={disableTxtEmail} ref={register({ required: true })} />            
           </Control>
+          { errors.txtEmail && <Help isColor='danger'>This field is required</Help> }
+          {/* <Help isColor='danger'>This email is already registered</Help> */}
         </Field>
         <Field>
           <Label>Mobile</Label>
           <Control>
-            <Input id="txtMobile" style={{ width: '50%' }}></Input>
+            <input name="txtMobile" className="input" type="tel" style={{ width: '50%' }} ref={register({ required: true })} />            
           </Control>
+          { errors.txtMobile && <Help isColor='danger'>This field is required</Help> }
         </Field>
         <Field>
           <Label>Address</Label>
           <Control>
-            <Input id="txtAddress" style={{ width: '50%' }}></Input>
+            <input name="txtAddress" className="input" type="text" style={{ width: '50%' }} ref={register({ required: true })} />
           </Control>
+          { errors.txtAddress && <Help isColor='danger'>This field is required</Help> }
         </Field>
         <Field>
           <Label>Product ID (s)</Label>
           <Control>
-            <Input id="txtProductIDs" style={{ width: '50%' }}></Input>
+            <input name="txtProductIDs" className="input" type="text" style={{ width: '50%' }} ref={register({ required: true })} />            
           </Control>
+          { errors.txtProductIDs && <Help isColor='danger'>This field is required</Help> }
         </Field>
         <Field>
           <Label>Source</Label>
           <Control>
-            <Select>
-              <option>Desktop</option>
-              <option>Mobile</option>
-            </Select>
+            <div className="select">
+              <select name="ddSource" ref={register({ required: true })}>
+                <option value="Desktop">Desktop</option>
+                <option value="Mobile">Mobile</option>
+              </select>
+            </div>            
           </Control>          
         </Field>
-        {/* <Field>
-          <Label>Created On:</Label>
-          <Control>
-            <Input id="txtCreated" style={{ width: '50%' }}></Input>
-          </Control>
-        </Field> */}
+
         <br />
         <div className="buttons">
-          <Button id="btnAdd"    isColor="primary" isOutlined onClick={insertCustomer}>Add</Button>
-          <Button id="btnUpdate" isColor="primary" isOutlined>Update</Button>
-          <Button id="btnDelete" isColor="danger"  isOutlined isActive>Remove</Button>
+          <Button id="btnAdd"    isColor="primary" isOutlined onClick={ handleSubmit(insertCustomer) } disabled={ disableBtnAdd    }>Add</Button>
+          <Button id="btnUpdate" isColor="primary" isOutlined onClick={ handleSubmit(updateCustomer) } disabled={ disableBtnUpdate }>Update</Button>
+          <Button id="btnDelete" isColor="danger"  isOutlined onClick={ handleSubmit(removeCustomer) } disabled={ disableBtnRemove }>Remove</Button>
         </div>
         <Title isSize={4} isDisplay="inline"  style={{ marginRight:'20px' }}>List of Customers</Title>
-        <Button isSize="small" onClick={getCustomers}>Refresh</Button>
-        <Subtitle isSize={6}>Last Updated: {lastUpdated}</Subtitle>
-        <Table id="tblCustomers" isStriped isNarrow className="is-hoverable" onClick={toggleActiveRow}>
+        <Button isSize="small" onClick={ getCustomers }>Refresh</Button>
+        <Subtitle isSize={6}>Last Updated: { lastUpdated }</Subtitle>
+        <Table id="tblCustomers" isStriped isNarrow className="is-hoverable" onClick={ toggleActiveRow }>
           { renderColumnNames() }
-          { renderColumnData() }
+          { renderColumnData()  }
         </Table>
       </Container>
     </Section>
